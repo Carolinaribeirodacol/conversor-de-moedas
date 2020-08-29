e08e66ad45399f893df23e5679d58c710ca87f84
Carolina Ribeiro Da Col Silva <43049988+Carolinaribeirodacol@users.noreply.github.com>
2020-08-29 17:13:53 -0300

    subindo aplicação

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Conversor from './components/conversor';
+import './app.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Conversor de moedas</h1>
+      <div className="conversores">
+        <Conversor moedaA="USD" moedaB="BRL"/>
+        <Conversor moedaA="BRL" moedaB="USD"/>
+      </div>
     </div>
   );
 }
